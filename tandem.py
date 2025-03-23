86b3bab20bc65ff4b38dd3c7ef028ea65fe60c5b
Sanhubny <sanhubny@gmail.com>
2025-03-24 03:41:13 +0800

    Add files via upload

---
+++ tandem.py
@@ -0,0 +1,15 @@
+word_data = {
+    "Tu as fait quoi?": ("你做了什麼？", "Tu as fait quoi ce week-end ?"),
+    "rêver": ("做夢、夢想", "Je veux toujours rêver en grand."),
+    "bruyant": ("吵鬧的（陽性）", "Le voisin est très bruyant le soir."),
+    "bruyante": ("吵鬧的（陰性）", "La rue bruyante m’empêche de dormir."),
+    "progresser": ("進步、提升", "Tu as bien progressé en français !"),
+    "un conseil": ("建議", "Peux-tu me donner un conseil ?"),
+    "gentil": ("親切的、友善的（陽性）", "Il est très gentil avec moi."),
+    "gentille": ("親切的、友善的（陰性）", "Elle est toujours gentille avec les enfants."),
+    "l’embouteillage": ("交通堵塞", "Il y a un grand embouteillage sur l’autoroute."),
+    "envoyer": ("寄送、發送", "Je vais envoyer un email ce soir."),
+    "quotidien": ("每日的；日常的（陽性）", "C’est mon journal quotidien."),
+    "quotidienne": ("每日的；日常的（陰性）", "Ma routine quotidienne commence à 7 heures."),
+    "améliorer": ("改善、提升", "Je veux améliorer mon français."),
+}
